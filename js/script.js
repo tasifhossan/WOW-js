b7ce89479bd116491c9f5f6032ff714fbacae585
Mohammad Tasif Hossan <153765460+tasifhossan@users.noreply.github.com>
2024-03-30 16:33:48 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// js
+
+wow = new WOW({
+  boxClass:     'wow',    
+  animateClass: 'animated', 
+  offset:       20,  
+  mobile:       true,   
+  live:         true    
+});
+wow.init();
+
+// jq
+$(document).ready(function(){
+
+    
+
+});
